ceecd74e50347907bef22e5250103e44b9f78ed8
Daehyeong Park <85856074+eoguddl@users.noreply.github.com>
2022-08-30 18:18:20 +0900

    Add files via upload

---
+++ spring-security-jwt-example/src/main/java/com/example/springsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.springsecurity.repository;
+
+import com.example.springsecurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
